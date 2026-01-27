71ec9be302baab52eaa5437ac9d34b5cc51a64e5
爱折腾的老家伙 <117909518+xxin83@users.noreply.github.com>
2026-01-27 13:08:05 +0800

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -1,45 +1,33 @@
 #!/bin/bash
 
-# define functions here
 cleanup_exit () {
-	# call this function to perform cleanup when a sanity check fails
+	echo "出现错误！正在执行清理。请再次运行脚本以安装 Waydroid。"
 	
-	echo Something went wrong! Performing cleanup. Run the script again to install waydroid.
-	
-	# remove installed packages
 	echo -e "$current_password\n" | sudo -S pacman -R --noconfirm libglibutil libgbinder \
 		python-gbinder waydroid wlroots cage wlr-randr binder_linux-dkms fakeroot debugedit \
 		dkms plymouth linux-neptune-$(uname -r | cut -d "-" -f5)-headers &> /dev/null
 	
-	# delete the waydroid directories
 	echo -e "$current_password\n" | sudo -S rm -rf ~/waydroid /var/lib/waydroid &> /dev/null
 	
-	# delete waydroid config and scripts
 	echo -e "$current_password\n" | sudo -S rm /etc/sudoers.d/zzzzzzzz-waydroid /etc/modules-load.d/waydroid.conf /usr/bin/waydroid* &> /dev/null
 
-	# delete Waydroid Toolbox and Waydroid Update symlinks
 	rm ~/Desktop/Waydroid-Updater &> /dev/null
 	rm ~/Desktop/Waydroid-Toolbox &> /dev/null
 
-	# delete Android_Waydroid folder and enable the readonly
 	echo -e "$current_password\n" | sudo -S rm -rf ~/Android_Waydroid &> /dev/null
 	echo -e "$current_password\n" | sudo -S steamos-readonly enable &> /dev/null
 	
-	# re-enable Decky Loader Plugin Loader service
 	if [ -f $PLUGIN_LOADER ]
 	then
-		echo Re-enabling the Decky Loader plugin loader service.
+		echo "正在重新启用 Decky Loader 插件服务。"
 		echo -e "$current_password\n" | sudo -S systemctl start plugin_loader.service
 	fi
 	
-	echo Cleanup completed. Please open an issue on the GitHub repo or leave a comment on the YT channel - 10MinuteSteamDeckGamer.
+	echo "清理完成。请在 GitHub 仓库提交 Issue 或在 YT 频道 - 10MinuteSteamDeckGamer 留言。"
 	exit
 }
 
 prepare_custom_image_location () {
-# call this function when deploying a custom Android image
-# custom Android images needs to be placed in /etc/waydroid-extra/images
-# this will create a symlink to /etc/waydroid-extra/images
 echo -e "$current_password\n" | sudo mkdir /etc/waydroid-extra &> /dev/null
 echo -e "$current_password\n" | sudo -S ln -s ~/waydroid/custom /etc/waydroid-extra/images &> /dev/null
 }
@@ -52,22 +40,20 @@ download_image () {
 	local name=$4
 	local hash
 
-	echo Downloading $name image
+	echo "正在下载 $name 镜像"
 	echo -e "$current_password\n" | sudo -S curl -o $dest_zip $src -L
 	hash=$(sha256sum "$dest_zip" | awk '{print $1}')
-	# Verify the hash
 	if [[ "$hash" != "$src_hash" ]]; then
-		echo sha256 hash mismatch for $name image, indicating a corrupted download. This might be due to a network error, you can try again.
+		echo "$name 镜像 sha256 校验不匹配，可能下载已损坏。这可能是由于网络错误，请重试。"
 		cleanup_exit
 	fi
 
-	echo Extracting Archive
+	echo "正在解压归档"
 	echo -e "$current_password\n" | sudo -S unzip -o $dest -d ~/waydroid/custom
 	echo -e "$current_password\n" | sudo -S rm $dest_zip
 }
 
 install_android_extras () {
-	# casualsnek / aleasto waydroid_script - install libndk and widevine
 	python3 -m venv $WAYDROID_SCRIPT_DIR/venv
 	$WAYDROID_SCRIPT_DIR/venv/bin/pip install -r $WAYDROID_SCRIPT_DIR/requirements.txt &> /dev/null
 
@@ -76,13 +62,11 @@ install_android_extras () {
 		echo -e "$current_password\n" | sudo -S $WAYDROID_SCRIPT_DIR/venv/bin/python3 $WAYDROID_SCRIPT_DIR/main.py -a13 install {libndk,widevine}
 	fi
 
-	echo casualsnek / aleasto waydroid_script done.
+	echo "casualsnek / aleasto waydroid_script 执行完毕。"
 	echo -e "$current_password\n" | sudo -S rm -rf $WAYDROID_SCRIPT_DIR
 	
-	# waydroid_base.prop - controller config and disable root
 	cat extras/waydroid_base.prop | sudo tee -a /var/lib/waydroid/waydroid_base.prop > /dev/null
 
-	# waydroid_base.prop fingerprint spoof - check if A11 or A13 and apply the spoof accordingly
 	if [ "$Choice" == "A13_NO_GAPPS" ] || [ "$Choice" == "A13_GAPPS" ] 
 	then
 		cat extras/android_spoof.prop | sudo tee -a /var/lib/waydroid/waydroid_base.prop > /dev/null
@@ -94,18 +78,17 @@ install_android_extras () {
 }
 
 check_waydroid_init () {
-	# check if waydroid initialization completed without errors
 	if [ $? -eq 0 ]
 	then
-		echo Waydroid initialization completed without errors!
+		echo "Waydroid 初始化完成，无错误！"
 
 	else
-		echo Waydroid did not initialize correctly.
-		echo This could be a hash mismatch / corrupted download.
-		echo This could also be a python issue. Attach this screenshot when filing a bug report!
-		echo Output of whereis python - $(whereis python)
-		echo Output of which python - $(which python)
-		echo Output of python version - $(python -V)
+		echo "Waydroid 未能正确初始化。"
+		echo "可能是哈希不匹配 / 下载损坏。"
+		echo "也可能是 Python 问题。提交错误报告时请附带此截图！"
+		echo "whereis python 输出 - $(whereis python)"
+		echo "which python 输出 - $(which python)"
+		echo "python 版本输出 - $(python -V)"
 
 		cleanup_exit
 	fi
